ec362c3990d4dc6f2d19d1400e5e6a7a93832ade
Gustavo Ferreira <102172136+gustapng@users.noreply.github.com>
2025-03-13 22:38:22 -0300

    update a feature

--- BookProject/Modules/Home/HomeView.swift
+++ BookProject/Modules/Home/HomeView.swift
@@ -10,9 +10,50 @@ import SwiftUI
 struct HomeView: View {
     @ObservedObject var presenter: HomePresenter
     var router: HomeRouter
+    @State private var search: String = ""
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            HStack {
+                Text("Olá, Usuário")
+                    .font(AppFonts.title)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Image(systemName: "bell")
+                    .resizable()
+                    .frame(width: 20, height: 20)
+                    .foregroundColor(Color("Orange"))
+            }
+
+            HStack {
+                Image(systemName: "magnifyingglass")
+                    .foregroundColor(.gray)
+                    .padding(.leading, 10)
+
+                TextField("Busque por Livros, Autores...", text: $search)
+                    .disableAutocorrection(true)
+                    .textInputAutocapitalization(.never)
+                    .padding()
+            }
+            .overlay(
+                RoundedRectangle(cornerRadius: 25)
+                    .stroke(Color("Gray"), lineWidth: 1)
+            )
+            
+            VStack(spacing: 20) {
+                HStack {
+                    Text("Descubra")
+                        .font(AppFonts.title)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    NavigationLink(destination: router.navigateToHome()) {
+                        Text("Ver Mais")
+                            .foregroundColor(AppColors.blue)
+                            .font(AppFonts.body)
+                    }
+                }
+            }
+        }
     }
 }
 
